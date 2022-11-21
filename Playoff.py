419c757923409d084b0c6a79a8528676edaf6814
fredriknystrom <fredriknystroms@gmail.com>
2022-11-21 16:46:08 +0100

    new format for playoffs

--- Playoff.py
+++ Playoff.py
@@ -21,8 +21,9 @@ class Playoff():
         self.generate_16()
         self.generate_quarterfinals()
         self.generate_semifinals()
-        self.generate_final()
-        self.generate_winner_and_top_scorer()
+        self.generate_bronze()
+        self.generate_gold()
+        self.generate_gold_bronze_and_top_scorer()
 
 
     def generate_16(self):
@@ -70,24 +71,26 @@ class Playoff():
                 row += 4
 
 
-    def generate_finals(self, n_teams, final_type, winner_func, col_offset):
+    def generate_finals(self, final_type, winner_func, col_offset):
         col = self.col_start + col_offset
         row = self.row_start
 
-        if final_type != 'Final':
+        if final_type not in ['Gold', 'Bronze']:
             cell = self.ws[get_cell(col, row)]
             set_value_to_cell(cell, final_type + 's', self.fill_color)
         row += 1
+        if final_type == 'Bronze':
+            row += 4
 
         teams = winner_func
-        for r in range(0, n_teams, 2):
+        for r in range(0, len(teams), 2):
 
             for c in range(4):
                 cell1 = self.ws[get_cell(col + c, row)]
                 cell2 = self.ws[get_cell(col + c, row+1)]
                 if c == 0:
-                    if final_type == 'Final':
-                        value1 = final_type
+                    if final_type == 'Gold' or 'Bronze':
+                        value1 = final_type + ' Match'
                     else:
                         value1 = f'{final_type} {int(r/2+1)}'
                     value2 = teams[r]
@@ -107,51 +110,70 @@ class Playoff():
 
 
     def generate_quarterfinals(self):
-        self.generate_finals(8, 'Quarterfinal', self.get_round_of_16_winners(), self.col_offset)
+        order = [20, 28, 4, 12, 24, 32, 16, 8]
+        winners = self.get_round_of_16_winners(order)
+        self.generate_finals('Quarterfinal', winners, self.col_offset)
 
 
     def generate_semifinals(self):
-        self.generate_finals(4, 'Semifinal', self.get_quaterfinal_winners(), 2*self.col_offset)
+        order = [4, 8, 12, 16]
+        winners = self.get_quaterfinal_winners(order)
+        self.generate_finals('Semifinal', winners, 2*self.col_offset)
+
+    def generate_bronze(self):
+        order = [4, 8]
+        losers = self.get_losers(order, 2*self.col_offset)
+        self.generate_finals('Bronze', losers, 3*self.col_offset)
 
 
-    def generate_final(self):
-        self.generate_finals(2, 'Final', self.get_semifinal_winners(), 3*self.col_offset)
+    def generate_gold(self):
+        order = [4, 8]
+        winners = self.get_semifinal_winners(order)
+        self.generate_finals('Gold', winners, 3*self.col_offset)
 
-
-    def generate_winner_and_top_scorer(self):
-        row = self.row_start + 5
+    def generate_gold_bronze_and_top_scorer(self):
+        row = self.row_start + 9
         col = self.col_start + 3*self.col_offset
-        values = [['Winner','x','Top Scorer', 'Goals Scored'],[self.get_final_winner(),'x','', '']]
-        for r in range(2):
-            for c in range(4):
-                value = values[r][c]
+        matrix = [['Gold Winner', 'Bronze Winner', 'Top Scorer', 'Goals Scored'],
+                  [self.get_winner([4]), self.get_winner([8]),'', '']]
+        for list in matrix:
+            for value in list:
                 if value != 'x':
-                    cell = self.ws[get_cell(col+c, row+r)]
-                    set_value_to_cell(cell, values[r][c], self.fill_color)
+                    cell = self.ws[get_cell(col, row)]
+                    set_value_to_cell(cell, value, self.fill_color)
+                col += 1
+            row +=1
+            col = self.col_start + 3*self.col_offset
 
-
-    def get_winners(self, n_winners, col_offset, final = 0):
+    def get_winners(self, rows, col_offset):
         col = self.col_start + col_offset
         winners = []
        
-        for i in range(n_winners):
-            row = self.row_start + i*4 + 2 - final
+        for row in rows:
             formula = f'=IF({get_cell(col+2, row)} > {get_cell(col+3, row)}, {get_cell(col, row)}, {get_cell(col+1, row)})'
             winners.append(formula)
+        return winners
 
+    def get_losers(self, rows, col_offset):
+        col = self.col_start + col_offset
+        winners = []
+       
+        for row in rows:
+            formula = f'=IF({get_cell(col+2, row)} < {get_cell(col+3, row)}, {get_cell(col, row)}, {get_cell(col+1, row)})'
+            winners.append(formula)
         return winners
 
 
-    def get_round_of_16_winners(self):
-        return self.get_winners(8, 0)
+    def get_round_of_16_winners(self, rows):
+        return self.get_winners(rows, 0)
     
 
-    def get_quaterfinal_winners(self):
-        return self.get_winners(4, self.col_offset)
+    def get_quaterfinal_winners(self, rows):
+        return self.get_winners(rows, self.col_offset)
 
 
-    def get_semifinal_winners(self):
-        return self.get_winners(2, 2*self.col_offset)
+    def get_semifinal_winners(self, rows):
+        return self.get_winners(rows, 2*self.col_offset)
 
-    def get_final_winner(self):
-        return self.get_winners(1, 3*self.col_offset)[0]
+    def get_winner(self, rows):
+        return self.get_winners(rows, 3*self.col_offset)[0]
